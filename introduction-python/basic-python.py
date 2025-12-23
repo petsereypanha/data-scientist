dd8dcf4dcec372c10907c2f4de3504ccd17e3fa0
petsereypanha <sereypanhapet@gmail.com>
2025-12-23 22:27:34 +0700

    first commit

---
+++ introduction-python/basic-python.py
@@ -0,0 +1,52 @@
+# Hit run code to see the output!
+print(5 / 8)
+
+# Addition and division
+print(4 + 5)
+print(10 / 2)
+
+# Subtraction
+print()
+
+# Multiplication
+print(5 - 5)
+print(3 * 5)
+
+# Create the variables monthly_savings and num_months
+monthly_savings=10
+num_months=4
+
+
+# Multiply monthly_savings and num_months
+new_savings = monthly_savings * num_months
+
+
+# Print new_savings
+print(new_savings)
+
+# Create a variable half
+half = 0.5
+
+# Create a variable intro
+intro = "Hello! How are you?"
+
+# Create a variable is_good
+is_good = True
+
+savings = 100
+new_savings = 40
+
+# Calculate total_savings using savings and new_savings
+total_savings = savings + new_savings
+print(total_savings)
+
+# Print the type of total_savings
+print(type(total_savings))
+
+intro = "Hello! How are you?"
+
+# Assign sum of intro and intro to doubleintro
+doubleintro = intro + intro
+
+# Print out doubleintro
+print(doubleintro)
