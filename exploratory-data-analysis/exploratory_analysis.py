37b79c31afc61ecd068c61f42270c16db649bfbe
petsereypanha <sereypanhapet@gmail.com>
2025-12-31 23:39:38 +0700

    Turning Exploratory Analysis into Action

---
+++ exploratory-data-analysis/exploratory_analysis.py
@@ -0,0 +1,64 @@
+# Import necessary libraries
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# load the salaries dataset
+salaries = pd.read_csv('../data/ds_salaries_clean.csv')
+
+# Print the relative frequency of Job_Category
+print(salaries['Job_Category'].value_counts(normalize=True))
+
+# Cross-tabulate Company_Size and Experience
+print(pd.crosstab(salaries["Company_Size"], salaries["Experience"]))
+
+# Cross-tabulate Job_Category and Company_Size
+print(pd.crosstab(salaries["Job_Category"], salaries["Company_Size"]))
+
+# Cross-tabulate Job_Category and Company_Size
+print(pd.crosstab(salaries["Job_Category"], salaries["Company_Size"],
+            values=salaries["Salary_USD"], aggfunc="mean"))
+
+# Get the month of the response
+salaries["month"] = salaries["date_of_response"].dt.month
+
+# Extract the weekday of the response
+salaries["weekday"] = salaries["date_of_response"].dt.weekday
+
+# Create a heatmap
+sns.heatmap(salaries.corr(numeric_only=True), annot=True)
+plt.show()
+
+# Find the 25th percentile
+twenty_fifth = salaries["Salary_USD"].quantile(0.25)
+
+# Save the median
+salaries_median = salaries["Salary_USD"].median()
+
+# Gather the 75th percentile
+seventy_fifth = salaries["Salary_USD"].quantile(0.75)
+print(twenty_fifth, salaries_median, seventy_fifth)
+
+# Create salary labels
+salary_labels = ["entry", "mid", "senior", "exec"]
+
+# Create the salary ranges list
+salary_ranges = [0, twenty_fifth, salaries_median, seventy_fifth, salaries["Salary_USD"].max()]
+
+# Create salary_level
+salaries["salary_level"] = pd.cut(salaries["Salary_USD"], bins=salary_ranges, labels=salary_labels)
+
+# Plot the count of salary levels at companies of different sizes
+sns.countplot(data=salaries, x="Company_Size", hue="salary_level")
+plt.show()
+
+# Filter for employees in the US or GB
+usa_and_gb = salaries[salaries["Employee_Location"].isin(["US", "GB"])]
+
+# Create a barplot of salaries by location
+sns.barplot(data=usa_and_gb, x="Employee_Location", y="Salary_USD")
+plt.show()
+
+# Create a bar plot of salary versus company size, factoring in employment status
+sns.barplot(data=salaries, x="Company_Size", y="Salary_USD", hue="Employment_Status")
+plt.show()
